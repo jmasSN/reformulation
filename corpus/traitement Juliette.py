1320f48494864f87e6c97a33bd71267d7d526858
jmasSN <juliette.massy@sorbonne-nouvelle.fr>
2023-05-02 16:46:49 +0200

    MAJ

--- corpus/traitement Juliette.py
+++ corpus/traitement Juliette.py
@@ -1,22 +1,55 @@
+# Comparer les exemplifications dans les 2 corpus (écrit (forum +masanté) vs oral)
+
+
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 
-tree = ET.parse('ecrit.xml')
+tree = ET.parse('corpus_final.xml')
 root = tree.getroot()
 
+# Nombres d'exemplifications total
+exempl,reste=0,0
+for child in root:
+    for segment in child:
+        if "rel_pragm" in segment.attrib and segment.attrib["rel_pragm"] == 'exempl':
+            exempl+=1
+            text = ''.join(child.itertext())
+            print(text)
+    reste+=1
+reste=reste-exempl
 
-
-patient,masante=0,0
 y=[]
 
-for child in root:
-    if child.attrib['forum'] == "patient":
-        patient +=1
-    else:
-        masante+=1
-y.append(patient)
-y.append(masante)
-mylabels = ["Forum patient", "Forum Masante"]
+print(f"Il y a {exempl} exemplifications sur {reste} reformulation.")
+
+y.append(exempl)
+y.append(reste)
+mylabels = ["Exempl", "Reformulation"]
 
 plt.pie(y, labels = mylabels, autopct = '%1.1f%%')
+plt.show() 
+
+# Nombres d'exemplifications par corpus
+
+ecrit,oral=0,0
+for child in root:
+    if child.attrib['mod']=='ecrit':
+        for segment in child:
+            if "rel_pragm" in segment.attrib and segment.attrib["rel_pragm"] == 'exempl':
+                ecrit+=1
+    if child.attrib['mod']=='oral':
+        for segment in child:
+            if "rel_pragm" in segment.attrib and segment.attrib["rel_pragm"] == 'exempl':
+                oral+=1
+
+
+refo=[]
+
+print(f"Il y a {ecrit} exemplifications dans les corpus écrit contre {oral} reformulation à l'oral.")
+
+refo.append(ecrit)
+refo.append(oral)
+mylabel = ["Ecrit", "Oral"]
+
+plt.pie(y, labels = mylabel, autopct = '%1.1f%%')
 plt.show() 
