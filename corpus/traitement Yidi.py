4012cc66122f23f5ba4ecb0b6815dc120736cc1c
yidi <yidi.huang.elodie@gmail.com>
2023-05-04 20:53:37 +0200

    ajout_pie_fonction

--- corpus/traitement Yidi.py
+++ corpus/traitement Yidi.py
@@ -8,22 +8,36 @@
 # Les exemplifications peuvent aussi être élargies aux dénominations, définitions et paraphrases (les erreurs d’annotations ?) à vérifier
 
 import re
+import matplotlib.pyplot as plt
 
-with open('corpus_final.txt','r') as t :
-    text=t.read()
+import re
+import matplotlib.pyplot as plt
 
-## trouver ce qui est dans les balises MRE
-    # l'expression pour trouver le contenu au milieu
-    mre=r'<MRE>(.*?)</MRE>'
-    resultat=re.findall(mre,text)
+with open('corpus_final.txt','r') as t:
+    text = t.read()
 
-    contenus=[]
-    # garder les expressions dans la liste en limitant les répétition
-    for mre in resultat:
-        if mre not in contenus:
-            contenus.append(mre)
-    print(f"{len(contenus)} MRE : {contenus}")
+# Trouver ce qui est dans les balises MRE
+mre_regex = r'<MRE>(.*?)</MRE>'
+resultats = re.findall(mre_regex, text)
 
+# Compter le nombre d'apparitions de chaque contenu MRE
+mre_comptes = {}
+for resultat in resultats:
+    if resultat in mre_comptes:
+        mre_comptes[resultat] += 1
+    else:
+        mre_comptes[resultat] = 1
+
+# Créer un pie chart de proportion basé sur les valeurs du dictionnaire
+labels = mre_comptes.keys()
+valeurs = mre_comptes.values()
+
+plt.pie(valeurs, labels=labels, autopct='%1.1f%%')
+plt.title("Proportion des contenus <MRE> dans le corpus")
+plt.show()
+print(len(mre_comptes.keys()),"types de <MRE> :",mre_comptes)
+
+#######################################################################################
 ## trouver les exemplification avec et sans <MRE>
 with open("corpus_final.txt",'r') as f :
     i,j=0,0
@@ -38,9 +52,19 @@ with open("corpus_final.txt",'r') as f :
             # mettre les cas inverse dans le compteur j
             else:
                 j+=1
+
+    # créer une liste de labels et une liste de données pour label
+    labels = ['Exempl avec <MRE>', 'Exempl sans <MRE>']
+    sizes = [i,j]
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+
+    plt.title("Proportion d'exemplifications avec ou sans <MRE>")
+    plt.show()
     print(f"Nb d'exemplifications avec <MRE> : {i} \n"
           f"Nb d'exemplifications sans <MRE> : {j}")
 
+
+########################################################################################
 ## trouver le nb de MRE pour 3 cats
 ## une fonction qui traite 3 parties de corpus (masante, patient, 3oral)
 def nb_MRE(mode):
@@ -51,9 +75,24 @@ def nb_MRE(mode):
             if re.search(fr'{mode}.*?(<MRE>)', ligne):
                 n += 1
         return n
-print("Nb de MRE dans masante :", nb_MRE('corpus="masante"'))
-print("Nb de MRE dans patient :", nb_MRE('corpus="patient"'))
-print("Nb de MRE dans corpus oral :", nb_MRE('mod="oral"'))
+
+# attacher la valeur à chaque catégorie
+nb1 =nb_MRE('corpus="masante"')
+nb2 =nb_MRE('corpus="patient"')
+nb3 =nb_MRE('mod="oral"')
+
+# affichage de résultat
+print("Nb de <MRE> dans masante :", nb1)
+print("Nb de <MRE> dans patient :", nb2)
+print("Nb de <MRE> dans corpus oral :", nb3)
+
+# construction de pie chart
+labels = ['<MRE> dans masante', '<MRE> dans patient', '<MRE> dans 3 corpus oraux']
+sizes = [nb1, nb2,nb3]
+plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+
+plt.title("Proportion de <MRE> dans 3 corpus")
+plt.show()
 
 
 
